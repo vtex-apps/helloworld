3dd8426c7e0a661be8233cf494d1e5750cfcf888
Andre Abrantes <andredossantosabrantes@gmail.com>
2019-07-03 10:12:08 -0300

    Init

---
+++ react/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class HelloWorld extends Component {
+  render() {
+    return "Hello World"
+  }
+}
+
+export default HelloWorld
